39ccc6f61986a342931c4f800da4b460e6298aaa
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2018-06-06 11:43:50 -0700

    add users controller func

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -4,16 +4,16 @@ const getUserHistory = async (req, res) => {
   const url = `https://api-rinkeby.etherscan.io/api?module=account&action=tokentx&contractaddress=${process.env.CADDRESS_POKE_SLOTS_TEST}&address=${req.params.address}&sort=asc&apikey=${process.env.ETHERSCAN_KEY}`;
   const result = await axios.get(url);
   const r = result.data.result;
+  r.forEach(tx => tx.confirmations = 1);
   res.status(200);
-  console.log(r);
   res.json({userHistory: r});
 };
 
 const getAllTransactions = async (req, res) => {
-  console.log("GOT HERE")
   const url = `http://api-rinkeby.etherscan.io/api?module=account&action=txlist&address=${req.params.address}&sort=asc&apikey=${process.env.ETHERSCAN_KEY}`
   const result = await axios.get(url);
   const r = result.data.result;
+  r.forEach(tx => tx.confirmations = 1);
   res.status(200);
   res.json({txList: r});
 };
@@ -23,4 +23,4 @@ const getAllTransactions = async (req, res) => {
 module.exports = {
   getUserHistory,
   getAllTransactions
-}
+};
