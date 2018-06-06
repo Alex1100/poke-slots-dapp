64ca036d63af316c93a70bbcc23fbf905129d11b
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2018-06-05 23:10:32 -0700

    update jest tests

--- api/middlewares/__tests__/ethMiddlewares.test.js
+++ api/middlewares/__tests__/ethMiddlewares.test.js
@@ -8,8 +8,7 @@ const provider = new HDWalletProvider(process.env.ACCOUNT_KEY, process.env.INFUR
 const web3 = new Web3(provider);
 const ownerAddress = process.env.OWNER_ADDR;
 const contractInstance = new web3.eth.Contract(abi, contractAddress);
-
-console.log("OWNER ADDR IS: ", ownerAddress);
+const secondEthAddress = process.env.ETHADDRESSTWO;
 
 
 const checkWalletFunds = async (req, res) => {
@@ -30,14 +29,15 @@ const checkWalletFunds = async (req, res) => {
 const exchangeEthForPSTToken = async (req, res) => {
   try {
     if (req.availableFunds > 0) {
-      const tx = await contractInstance.methods.transfer(req.body.toAddr, req.body.value)
-                      .call({
-                        from: req.body.fromAddr,
-                        to: req.body.toAddr,
-                        value: req.body.value
-                      });
-      console.log("Transaction is: ", tx);
-      req.txSent = tx;
+      const tx = await contractInstance
+                  .methods
+                  .transfer(req.body.toAddr, req.body.value)
+
+      const res = await tx
+                  .call({ gasPrice: '51000000000', from: req.body.fromAddr });
+
+      console.log("Transaction is: ", res);
+      req.txSent = res;
       return req.txSent;
     } else {
       throw Error('Insufficient Funds');
@@ -52,14 +52,14 @@ const exchangeEthForPSTToken = async (req, res) => {
 
 describe('Eth Middleware Functions', () => {
   it('tests the `checkWalletFunds` func', async () => {
-    const fundsResult = await checkWalletFunds({ body: { fromAddr: process.env.OWNER_ADDR } }, {});
+    const fundsResult = await checkWalletFunds({ body: { fromAddr: ownerAddress } }, {});
     expect(typeof fundsResult).toEqual('string');
     expect(typeof parseInt(fundsResult)).toEqual('number');
     expect(parseInt(fundsResult)).toBeGreaterThan(0);
   });
 
-  // it('tests the `exchangeEthForPSTToken` func', async () => {
-  //   const txRes = await exchangeEthForPSTToken({ availableFunds: 1, body: { toAddr: contractAddress, fromAddr: ownerAddress, value: 100} }, {});
-  //   assert(txRes).toMatchSnapshot();
-  // });
+  it('tests the `exchangeEthForPSTToken` func', async () => {
+    const txRes = await exchangeEthForPSTToken({ availableFunds: 1, body: { toAddr: secondEthAddress, fromAddr: ownerAddress, value: 100 } }, {});
+    expect(txRes).toMatchSnapshot();
+  });
 });
